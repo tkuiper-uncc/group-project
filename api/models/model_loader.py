79be16d06dd467d60b81fcf542d3be8aa0f461c7
soonpepper <vmadapak@charlotte.edu>
2025-07-22 23:02:26 -0400

    Simplified model_loader.py
    Condensed all base class calls through importing base

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,11 +1,13 @@
 from . import orders, order_details, recipes, sandwiches, resources
 
-from ..dependencies.database import engine
+from ..dependencies.database import Base, engine
 
 
 def index():
-    orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+    # Uses all bases in classes to create all tables
+    Base.metadata.create_all(bind=engine)
+    ### orders.Base.metadata.create_all(engine)
+    ### order_details.Base.metadata.create_all(engine)
+    ### recipes.Base.metadata.create_all(engine)
+    ### sandwiches.Base.metadata.create_all(engine)
+    ### resources.Base.metadata.create_all(engine) 
